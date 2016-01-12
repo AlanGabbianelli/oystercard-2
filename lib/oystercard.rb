e4732b7cdf3e3715616a470ec9c878f891a649a6
Alan Gabbianelli <alan.gabbianelli@gmail.com>
2016-01-12 10:44:03 +0000

    Can deduct money + cannot go below zero

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,19 @@
 class Oystercard
-attr_reader :balance
-MAX_BALANCE = 50
+  attr_reader :balance
+  MAX_BALANCE = 50
+  MIN_BALANCE = 0
 
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
-    fail "Max balance: £#{MAX_BALANCE} reached" if @balance + amount > MAX_BALANCE
+    fail "Max balance #{MAX_BALANCE} reached" if @balance + amount > MAX_BALANCE
     @balance += amount
   end
 
+  def deduct(amount)
+    fail "Cannot go below #{MIN_BALANCE}" if @balance - amount < MIN_BALANCE
+    @balance -= amount
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,6 @@
 require 'oystercard'
 describe Oystercard do
-let(:oystercard) { described_class.new }
+  let(:oystercard) { described_class.new }
 
   it 'has a balance' do
     expect(oystercard.balance).to eq(0)
@@ -11,8 +11,18 @@ let(:oystercard) { described_class.new }
   end
 
   it 'has a maximum balance' do
-    error = "Max balance: £#{Oystercard::MAX_BALANCE} reached"
+    error = "Max balance #{Oystercard::MAX_BALANCE} reached"
     oystercard.top_up(50)
     expect { oystercard.top_up(1) }.to raise_error error
+  end
+
+  it 'can have money deducted' do
+    oystercard.top_up(50)
+    expect { oystercard.deduct(20) }.to change { oystercard.balance }.by(-20)
+  end
+
+  it 'cannot go below 0' do
+    error = "Cannot go below #{Oystercard::MIN_BALANCE}"
+    expect { oystercard.deduct(1) }.to raise_error error
   end
 end
